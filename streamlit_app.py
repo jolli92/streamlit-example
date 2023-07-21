ef419feb2ea2ec9667d9be22434bf335c2a47d9b
jolli92 <139642927+jolli92@users.noreply.github.com>
2023-07-22 00:09:30 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,7 @@ import seaborn as sns
 import streamlit as st
 import plotly.express as px
 import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 
 
 option = st.sidebar.selectbox(

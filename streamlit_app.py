2c6d460eefd4a0e1d28229702aa79c6569ad7069
jolli92 <139642927+jolli92@users.noreply.github.com>
2023-07-21 19:43:47 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,7 @@ option = st.sidebar.selectbox(
 st.title('Analyse de bank marketing')
 df_file = st.sidebar.file_uploader("Upload a Dataset", type=['csv', 'txt'])
 if option == 'Étude Statistiques':
+    df = pd.read_csv(df_file)
     st.header('Distribution de la variable cible')        
     sns.countplot(x="deposit", data=df)
     st.pyplot(plt.gcf())

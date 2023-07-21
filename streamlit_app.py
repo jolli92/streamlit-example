852a017c4aa5694fb34b7e7cb0b4cc66c236c1c8
jolli92 <139642927+jolli92@users.noreply.github.com>
2023-07-22 00:25:32 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,6 @@ if option == 'Étude Statistiques':
     labels = deposit_counts.index
     sizes = deposit_counts.values
     fig2 = go.Figure(data=go.Pie(labels=labels, values=sizes, textinfo='percent+label', insidetextorientation='radial'))
-    fig2.update_layout(title='Répartition des dépôts')
     col1, col2 = st.columns(2)
     col1.plotly_chart(fig1, use_container_width=True)
     col2.plotly_chart(fig2, use_container_width=True)

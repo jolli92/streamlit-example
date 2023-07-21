2430f53100c5c7d060587fa8304e0de6c3542644
jolli92 <139642927+jolli92@users.noreply.github.com>
2023-07-21 18:40:05 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,12 +12,26 @@ df = pd.read_csv(file_path)
 
 st.title('Analyse de bank marketing')
 df = st.file_uploader("Upload a Dataset", type=['csv', 'txt'])
-if df is not None:
-    df = pd.read_csv(df)
-    st.dataframe(df.head())
-    if st.button("Show summary"):
-        st.write(df.describe())
-    if st.button("Show Histogram"):
-        st.write(sns.histplot(df))
-        st.plt.show()
+if df_file is not None:
+    df = pd.read_csv(df_file)
+    st.sidebar.dataframe(df.head())
+
+    st.title('Etude statistique')
+
+    st.header('Distribution de la variable cible')
+    plt.figure(figsize=(6,4))
+    sns.countplot(x="deposit", data=df)
+    st.pyplot()
+
+    # Ajoutez ici les autres graphiques, en suivant le même format. Par exemple:
+
+    st.header('Souscription à un compte à terme')
+    deposit_counts = df['deposit'].value_counts()
+    labels = deposit_counts.index
+    sizes = deposit_counts.values
+    plt.figure(figsize=(6,4))
+    plt.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=90)
+    plt.axis('equal')
+    plt.title('Répartition des dépôts')
+    st.pyplot()
 

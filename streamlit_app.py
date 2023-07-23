da9024c4ff6a9b61ada57ee50d100a6e314c2ff3
jolli92 <139642927+jolli92@users.noreply.github.com>
2023-07-23 18:20:10 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,6 @@ if option == 'Etude statistiques':
     if option == 'Analyse des informations brutes':
         st.write("Informations du DataFrame :")
         info_str = df.info().to_string()
-        st.write("Informations du DataFrame :")
         st.text(info_str)
         st.write("Description statistique du DataFrame :")
         st.dataframe(df.describe())

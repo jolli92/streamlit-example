da8e9f7dfb8498cfe68cb184553ec3b77aba1a1a
jolli92 <139642927+jolli92@users.noreply.github.com>
2023-07-23 17:35:39 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -70,6 +70,8 @@ if option == 'Étude Statistiques':
         col2.plotly_chart(fig, use_container_width=True)
         st.write("Les clients ayant des emplois de gestion et des emplois d'ouvrier qualifié sont les plus nombreux dans la banque.")
         st.write("Il y a très peu d'étudiants parmi les clients de la banque.")
+        st.write("Management et blue colar sont les métiers les plus représentés chez les clients de la banque.")
+        st.write("La majorité des clients de la banques sont mariés (56.9%) ou celibataire (31.5%).")
 
         
         marital_counts = df['marital'].value_counts()

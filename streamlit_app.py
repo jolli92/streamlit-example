e0ce971c56d2a000c8bd867542c30be9f3462a38
jolli92 <139642927+jolli92@users.noreply.github.com>
2023-10-28 02:02:58 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -545,3 +545,6 @@ if page == pages[3] :
 
 if page == pages[4] :
     st.write("Prédictions_2")
+    st.write("Ajout d'une colonne prénom + téléphone (généré aleatoirement) et redistribution compléte du dataset sur toutes les colonnes à l'aide de .sample")
+    df = pd.read_csv('Banktest.csv')
+    st.dataframe(df)

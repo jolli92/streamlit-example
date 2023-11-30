2a348224c9a453d18300786dc27f5750204b7b3b
jolli92 <139642927+jolli92@users.noreply.github.com>
2023-11-30 11:38:43 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,9 +22,7 @@ from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import learning_curve
 from sklearn.metrics import r2_score
 from joblib import load
-import warnings
-warnings.filterwarnings("ignore")
-
+st.set_option('deprecation.showPyplotGlobalUse', False)
 
 df = pd.read_csv('bank.csv')
 st.title("Analyse de bank marketing")

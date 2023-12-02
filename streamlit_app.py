1e5608dc1a9605899dad863928a9f78f3b114ebc
jolli92 <139642927+jolli92@users.noreply.github.com>
2023-12-03 00:20:00 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -387,7 +387,7 @@ if page == pages[2] :
        train_sizes, train_scores, test_scores = learning_curve(clf_dt_ginis, X_train_encoded, y_train, n_jobs=-1, 
                                                             train_sizes=np.linspace(.1, 1.0, 5))
     if model_choisi == 'Random Forest' :
-       #model = load('clf_optimized.pkl')
+       #model = load('clf_optimized')
        #model.fit(X_train, y_train)
        with open("clf_optimized", "rb") as file:
            clf_optimizedd = pickle.load(file)

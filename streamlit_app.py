a5a76b7a791893a84e187fda1c1fb0ed5c21f9a7
jolli92 <139642927+jolli92@users.noreply.github.com>
2023-12-02 21:11:09 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -348,35 +348,35 @@ if page == pages[2] :
         
     if model_choisi == 'Regression Logistique' :
         model = load('LogisticRegression.joblib')
-        model.fit(X_train, y_train)
+        #model.fit(X_train, y_train)
         y_pred = model.predict(X_test)
         st.text(classification_report(y_test, y_pred))
         train_sizes, train_scores, test_scores = learning_curve(model, X_train, y_train, n_jobs=-1, 
                                                         train_sizes=np.linspace(.1, 1.0, 5))
     if model_choisi == 'KNN' :
         model = load('knn_ma.joblib')
-        model.fit(X_train, y_train)
+        #model.fit(X_train, y_train)
         y_pred = model.predict(X_test)
         st.text(classification_report(y_test, y_pred))
         train_sizes, train_scores, test_scores = learning_curve(model, X_train, y_train, n_jobs=-1, 
                                                         train_sizes=np.linspace(.1, 1.0, 5))
     if model_choisi == 'Decision Tree' :
         model = load('clf_dt_gini.pkl')
-        model.fit(X_train, y_train)
+       #model.fit(X_train, y_train)
         y_pred = model.predict(X_test)
         st.text(classification_report(y_test, y_pred))
         train_sizes, train_scores, test_scores = learning_curve(model, X_train, y_train, n_jobs=-1, 
                                                             train_sizes=np.linspace(.1, 1.0, 5))
     if model_choisi == 'Random Forest' :
         model = load('clf_optimized.pkl')
-        model.fit(X_train, y_train)
+        #model.fit(X_train, y_train)
         y_pred = model.predict(X_test)
         st.text(classification_report(y_test, y_pred))
         train_sizes, train_scores, test_scores = learning_curve(model, X_train, y_train, n_jobs=-1, 
                                                             train_sizes=np.linspace(.1, 1.0, 5))
     if model_choisi == 'XGBoost' :
         model = load('XGboost.joblib')
-        model.fit(X_train, y_train)
+        #model.fit(X_train, y_train)
         y_pred = model.predict(X_test)
         st.text(classification_report(y_test, y_pred))
         train_sizes, train_scores, test_scores = learning_curve(model, X_train, y_train, n_jobs=-1, 

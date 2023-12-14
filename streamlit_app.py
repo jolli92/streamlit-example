7df879f5f02d422bfd22c1aa3bbfa34452951274
jolli92 <139642927+jolli92@users.noreply.github.com>
2023-12-14 21:00:51 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1616,7 +1616,7 @@ if page == pages[5] :
   st.write(summary2)
   with st.container():
         selected_vars = st.multiselect('Sélectionnez les variables à visualiser:', filtered_df.columns)
-        show_annotations = st.checkbox('Afficher les commentaires')
+        #show_annotations = st.checkbox('Afficher les commentaires')
   if selected_vars:
         fig = create_visualisations(filtered_df, selected_vars)
         st.plotly_chart(fig)
